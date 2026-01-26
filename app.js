e93ad88252d1208caf997e136f1577aee3fb5f29
loops <128599880+loopsc@users.noreply.github.com>
2026-01-26 17:05:42 +1300

    Install dependencies Set up project structure Database created

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const booksRouter = require("./routes/booksRouter");
+
+// For rendering ejs files
+const path = require("node:path");
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+//For working with forms
+app.use(express.urlencoded({ extended: true }));
+// For serving CSS
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
+
+app.use("/", booksRouter);
+
+app.listen(3000, (error) => {
+    if (error) {
+        throw error;
+    }
+    console.log("Server started successfully")
+})
---
+++ routes/booksRouter.js
@@ -0,0 +1,5 @@
+const { Router } = require("express");
+const booksRouter = Router();
+// Todo: Import controller
+
+module.exports = booksRouter;
